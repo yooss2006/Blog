9fc90f41d81ac286da83f273061c99e72804d506
yooss2006 <yooss2006@naver.com>
2024-08-12 20:51:58 +0900

    Feat: shadcn 환경 설정

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <main>긱 갱스터 블로그</main>;
+  return (
+    <main>
+      긱 갱스터 블로그<Button>하이</Button>
+    </main>
+  );
 }
